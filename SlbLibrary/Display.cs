c1eb8eb38a026c1e5fe881899ef5adea5d256c0a
Zhang Yue <zhangyue02135@yahoo.com.cn>
2012-10-17 16:43:23 +0800

    Added Display class to show message

---
+++ SlbLibrary/Display.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SlbLibrary
+{
+    static internal class Display
+    {
+        public static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
