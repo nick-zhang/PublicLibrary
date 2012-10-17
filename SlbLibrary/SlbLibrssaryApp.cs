addbce49319e5183c4cc86d22bb7dc00f9f2bece
Zhang Yue <zhangyue02135@yahoo.com.cn>
2012-10-17 16:42:37 +0800

    Show start message on console

--- SlbLibrary/SlbLibrssaryApp.cs
+++ SlbLibrary/SlbLibrssaryApp.cs
@@ -1,9 +1,15 @@
-﻿namespace SlbLibrary
+﻿using System;
+
+namespace SlbLibrary
 {
     class SlbLibrssaryApp
     {
         static void Main(string[] args)
         {
+            var library = new Library();
+            var message = library.Start();
+            Display.ShowMessage(message);
+            Console.ReadKey();
         }
     }
 }
